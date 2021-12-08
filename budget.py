714a9a49a006227e06fbe66c56ed815bb6f7f6a4
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-08 13:40:31 +0800

    Add init method to the class category

---
+++ budget.py
@@ -0,0 +1,15 @@
+class Category:
+    def __init__(self.name):
+        self.ledger = []
+        self.name = name
+        self.total = 0
+    
+
+
+
+
+
+
+
+
+# def create_spend_chart(categories):
