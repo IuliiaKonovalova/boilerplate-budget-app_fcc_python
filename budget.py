a9b7e8436715787e778a075985a74f10a9abae68
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-08 13:45:23 +0800

    Add deposit method to the function

--- budget.py
+++ budget.py
@@ -1,9 +1,22 @@
 class Category:
-    def __init__(self.name):
+    def __init__(self, name):
         self.ledger = []
         self.name = name
         self.total = 0
     
+    def deposit(self, amount, description=None):
+        self.total += amount
+        if description == None:
+            self.description = ""
+        else:
+            self.description = description
+        self.ledger.append({
+            "amount": amount,
+            "description": self.description
+        })
+
+    
+
 
 
 
